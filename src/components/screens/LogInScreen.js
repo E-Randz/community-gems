902cefb865b95844f6a1c96f7d59316cc3cca462
liamriley123 <liamriley123@yahoo.co.uk>
2019-02-19 12:54:24 +0000

    create sign in function

--- src/components/screens/LogInScreen.js
+++ src/components/screens/LogInScreen.js
@@ -1,38 +1,60 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet,  } from 'react-native';
-import { Input } from '../Input';
-import { Button } from '../Button'
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Input } from "../Input";
+import { Button } from "../Button";
+import { auth, signInWithEmailAndPassword } from "firebase";
 
 class LogInScreen extends Component {
   state = {
-    email: '',
-    password: '',
-  }
-  componentDidUpdate() {
-    console.log(this.state);
-  }
+    email: "",
+    password: ""
+  };
+
+  LogInUser = (email, password) => {
+    try {
+      auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(function(user) {
+          console.log(user);
+        });
+    } catch (err) {
+      console.log(err.toString());
+    }
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Input 
-             placeholder="Enter your email..."
-             label="Email Address"
-             onChangeText={(email) => this.setState({email})}
-             name='email'
-             value={this.state.email}
+        <Input
+          placeholder="Enter your email..."
+          label="Email Address"
+          onChangeText={email => this.setState({ email })}
+          name="email"
+          value={this.state.email}
         />
         <Input
           placeholder="Enter your password..."
           label="Password"
           secureTextEntry
-          name='password'
-          onChangeText={(password) => this.setState({password})}
+          name="password"
+          onChangeText={password => this.setState({ password })}
           value={this.state.password}
         />
-        <Button onPress={() => this.LogInUser(this.state.email, this.state.password)}> Log In </Button>
-        <Button onPress={() => this.props.navigation.navigate('SignUp')}>Sign Up</Button>
-        <Button onPress={() => this.props.navigation.navigate('ForgotPassword')}> Forgot Password </Button>
+        <Button
+          onPress={() => this.LogInUser(this.state.email, this.state.password)}
+        >
+          {" "}
+          Log In{" "}
+        </Button>
+        <Button onPress={() => this.props.navigation.navigate("SignUp")}>
+          Sign Up
+        </Button>
+        <Button
+          onPress={() => this.props.navigation.navigate("ForgotPassword")}
+        >
+          {" "}
+          Forgot Password{" "}
+        </Button>
       </View>
     );
   }
@@ -44,18 +66,17 @@ class LogInScreen extends Component {
   //   })
   // }
   LogInUser = () => {
-    console.log('log in')
-    this.props.navigation.navigate('App');
-  }
-
+    console.log("log in");
+    this.props.navigation.navigate("App");
+  };
 }
 export default LogInScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
