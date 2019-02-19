a0a5e5c5e76d44f4aaf4acc6e66b993ee086a60b
liamriley123 <liamriley123@yahoo.co.uk>
2019-02-19 12:54:34 +0000

    create sign up function

--- src/components/screens/SignUpScreen.js
+++ src/components/screens/SignUpScreen.js
@@ -1,24 +1,90 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Input } from "../Input";
+import * as firebase from "firebase";
+import { Button } from "../Button";
 
 class SignUpScreen extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    house_number: "",
+    postcode: ""
+  };
+
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBKQA51q2geGB13b9S7pllwZTZRK8AWNkU",
+      authDomain: "community-gems.firebaseapp.com",
+      databaseURL: "https://community-gems.firebaseio.com",
+      projectId: "community-gems",
+      storageBucket: "community-gems.appspot.com"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  signUpUser = () => {
+    const { email, password } = this.state;
+    try {
+      if (this.state.password.length < 6) {
+        alert("Please enter atleast 6 characters");
+        return;
+      }
+      firebase.auth().createUserWithEmailAndPassword(email, password);
+    } catch (err) {
+      console.log(err.toString());
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Sign Up Screen</Text>
+        <Text>Home Screen </Text>
+        <Input
+          placeholder="please insert username"
+          label="username"
+          onChangeText={username => this.setState({ username })}
+          value={this.state.username}
+        />
+        <Input
+          placeholder="please insert email"
+          label="email"
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+        <Input
+          placeholder="please insert password"
+          label="password"
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+        <Input
+          placeholder="please insert house number"
+          label="house_number"
+          onChangeText={house_number => this.setState({ house_number })}
+          value={this.state.house_number}
+        />
+        <Input
+          placeholder="please insert postcode"
+          label="postcode"
+          onChangeText={postcode => this.setState({ postcode })}
+          value={this.state.postcode}
+        />
+        <Button onPress={this.signUpUser}>Add User</Button>
       </View>
     );
   }
 }
-
 
 export default SignUpScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
