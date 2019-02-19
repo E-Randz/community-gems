9443d4b6ed2c3e08c44c60fedf8741068f71c76b
Emma Randall <eerandall1@gmail.com>
2019-02-19 10:30:40 +0000

    create auth navigator

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation';
 import TabNavigator from './src/navigation/TabNavigator';
-import AuthLoadingScreen from './src/components/screens/AuthLoadingScreen'
+import AuthLoadingScreen from './src/components/screens/AuthLoadingScreen';
+import AuthNavigator from './src/navigation/AuthNavigator';
 
 const switchNavigator = createSwitchNavigator({
   AuthLoading: AuthLoadingScreen,
   App: TabNavigator,
-  Auth: AuthenticationNavigator
+  Auth: AuthNavigator
 },
 {
   initialRouteName: 'App'
--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -1,5 +1,13 @@
-import { createBottomTabNavigator } from 'react-navigation';
-import HomeScreen from '../components/screens/HomeScreen';
-import MessageScreen from '../components/screens/MessageScreen';
-import CreateEventScreen from '../components/screens/CreateEventScreen';
+import { createStackNavigator } from 'react-navigation';
+import LogInScreen from '../components/screens/HomeScreen';
+import SignUpScreen from '../components/screens/MessageScreen';
+import ForgotPasswordScreen from '../components/screens/CreateEventScreen';
+
+const AuthNavigator = createStackNavigator({
+  LogIn: LogInScreen,
+  SignUp: SignUpScreen,
+  ForgotPassword: ForgotPasswordScreen,
+})
+
+export default AuthNavigator;
 
