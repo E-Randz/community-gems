e5170c87f23d41f58a4f2acdcabfeb4b41108fe3
Emma Randall <eerandall1@gmail.com>
2019-02-19 10:18:44 +0000

    create swicth navigator in app.js

--- App.js
+++ App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation';
 import TabNavigator from './src/navigation/TabNavigator';
+import AuthLoadingScreen from './src/components/screens/AuthLoadingScreen'
+
+const switchNavigator = createSwitchNavigator({
+  AuthLoading: AuthLoadingScreen,
+  App: TabNavigator
+},
+{
+  initialRouteName: 'AuthLoading'
+}
+)
 
 
 
-export default createAppContainer(TabNavigator);
+export default createAppContainer(switchNavigator);
