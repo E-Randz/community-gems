d43b32d1c9900f8c384be96503ef80f9c2233b62
liamriley123 <liamriley123@yahoo.co.uk>
2019-02-19 14:31:16 +0000

    add initialise firebase to app

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation';
-import TabNavigator from './src/navigation/TabNavigator';
-import AuthLoadingScreen from './src/components/screens/AuthLoadingScreen';
-import AuthNavigator from './src/navigation/AuthNavigator';
+import React from "react";
+import { Text, View } from "react-native";
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createSwitchNavigator
+} from "react-navigation";
+import TabNavigator from "./src/navigation/TabNavigator";
+import AuthLoadingScreen from "./src/components/screens/AuthLoadingScreen";
+import AuthNavigator from "./src/navigation/AuthNavigator";
+import * as firebase from "firebase";
 
-const switchNavigator = createSwitchNavigator({
-  AuthLoading: AuthLoadingScreen,
-  App: TabNavigator,
-  Auth: AuthNavigator
-},
-{
-  initialRouteName: 'Auth'
-}
-)
+const firebaseConfig = {
+  apiKey: "AIzaSyBKQA51q2geGB13b9S7pllwZTZRK8AWNkU",
+  authDomain: "community-gems.firebaseapp.com",
+  databaseURL: "https://community-gems.firebaseio.com",
+  projectId: "community-gems",
+  storageBucket: "community-gems.appspot.com"
+};
+firebase.initializeApp(firebaseConfig);
 
-
+const switchNavigator = createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: TabNavigator,
+    Auth: AuthNavigator
+  },
+  {
+    initialRouteName: "Auth"
+  }
+);
 
 export default createAppContainer(switchNavigator);
