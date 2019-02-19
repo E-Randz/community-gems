073285ffca0ca4eb7c32d44935fb97d6bb2d44ed
Emma Randall <eerandall1@gmail.com>
2019-02-19 10:42:25 +0000

    fix authnavigator import sources

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const switchNavigator = createSwitchNavigator({
   Auth: AuthNavigator
 },
 {
-  initialRouteName: 'App'
+  initialRouteName: 'Auth'
 }
 )
 
--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -1,13 +1,18 @@
 import { createStackNavigator } from 'react-navigation';
-import LogInScreen from '../components/screens/HomeScreen';
-import SignUpScreen from '../components/screens/MessageScreen';
-import ForgotPasswordScreen from '../components/screens/CreateEventScreen';
+import LogInScreen from '../components/screens/LogInScreen';
+import SignUpScreen from '../components/screens/SignUpScreen';
+import ForgotPasswordScreen from '../components/screens/ForgotPasswordScreen';
 
 const AuthNavigator = createStackNavigator({
   LogIn: LogInScreen,
   SignUp: SignUpScreen,
   ForgotPassword: ForgotPasswordScreen,
-})
+},
+{
+  initialRouteName: 'LogIn',
+}
+
+)
 
 export default AuthNavigator;
 
