cc1740319f9fec7bbe2e93b741483c4758724053
Emma <38498590+E-Randz@users.noreply.github.com>
2019-02-19 16:56:32 +0000

    Delete config.js

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-export default config = {
-  apiKey: "AIzaSyBKQA51q2geGB13b9S7pllwZTZRK8AWNkU",
-  authDomain: "community-gems.firebaseapp.com",
-  databaseURL: "https://community-gems.firebaseio.com",
-  projectId: "community-gems",
-  storageBucket: "community-gems.appspot.com",
-  messagingSenderId: "913262966255"
-};
