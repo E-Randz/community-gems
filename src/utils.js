ce7c09fc7d42c7a363b58856d61f848c0d770726
John Nezzi <john@Johns-MacBook-Pro.local>
2019-02-20 15:37:49 +0000

    write getCoords function

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,12 @@ const opencage = require('opencage-api-client')
 
 const getCords = (address) => {
   opencage.geocode(address) 
+  .then(data => {
+        coords = data.results.geometry
+        return coords
+        }
+    )
+    .catch(error => { console.log('error', error.message)})
 }
 
 export default getCords
