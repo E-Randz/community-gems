510a32b31965ea3fadf9db71423bf0ece44f5697
Liamriley123 <43750285+Liamriley123@users.noreply.github.com>
2019-02-20 16:32:38 +0000

    Reviews profile (#15)
    * change home screen
    
    * list of hardcoded reviews mapped for profile page

--- src/components/screens/ProfileScreen.js
+++ src/components/screens/ProfileScreen.js
@@ -1,18 +1,62 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+import { ListItem } from "react-native-elements";
+
+const reviews = [
+  {
+    comment_id: 1,
+    comment_creator: "liam",
+    comment_body: "great guy, great work",
+    comment_votes: "100",
+    comment_date: Date.now()
+  },
+  {
+    comment_id: 2,
+    comment_creator: "Mo",
+    comment_body: "hard working volunteer",
+    comment_votes: "5678943",
+    comment_date: Date.now()
+  },
+  {
+    comment_id: 4,
+    comment_creator: "Flav",
+    comment_body: "hard working volunteer hard wor",
+    comment_votes: "5678943",
+    comment_date: Date.now()
+  },
+  {
+    comment_id: 5,
+    comment_creator: "Yas",
+    comment_body: "djodi",
+    comment_votes: "5678943",
+    comment_date: Date.now()
+  },
+  {
+    comment_id: 3,
+    comment_creator: "john",
+    comment_body: "e3jhirui34r",
+    comment_votes: "3",
+    comment_date: Date.now()
+  }
+];
 
 export default class Profile extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <View style={styles.header} />
         <Image
           style={styles.avatar}
           source={{ uri: "https://bootdey.com/img/Content/avatar/avatar6.png" }}
         />
         <View style={styles.body}>
-          {/* <View style={styles.bodyContent}> */}
-
           <Text style={styles.name}>John Doe</Text>
           <Text style={styles.info}>Gems: 5💎</Text>
           <Text style={styles.description}>
@@ -25,9 +69,22 @@ export default class Profile extends Component {
             <Text> Edit Info</Text>
           </TouchableOpacity>
         </View>
-      </View>
-      //   </View>
-
+        <View style={styles.reviewHolder}>
+          {reviews.map((list, i) => (
+            <ListItem
+              key={i}
+              leftAvatar={{
+                source: {
+                  uri: "https://bootdey.com/img/Content/avatar/avatar6.png"
+                }
+              }}
+              title={list.comment_creator}
+              subtitle={list.comment_body}
+              style={styles.reviewBox}
+            />
+          ))}
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -56,9 +113,26 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 22,
-
     color: "black",
     fontWeight: "600"
+  },
+  reviewBox: {
+    fontSize: 6,
+    backgroundColor: "#00BFFF",
+    fontWeight: "600",
+    borderColor: "#00BFFF",
+    borderWidth: 2,
+    marginTop: 2
+  },
+  //   reviewHolder: {
+  //     backgroundColor: "#00BFFF"
+  //   },
+  reviewBox: {
+    fontSize: 6,
+    backgroundColor: "#00BFFF",
+    fontWeight: "600",
+    borderBottomColor: "#00BFFF",
+    borderBottomWidth: 1
   },
   body: {
     marginTop: 70,
