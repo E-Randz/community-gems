0e7d44a80969660a5e8478362a6ac02f72ebaa53
liamriley123 <liamriley123@yahoo.co.uk>
2019-02-20 12:26:13 +0000

    Adapt Chat file to show messages coming in and user messages history

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,8 @@
 import React from "react";
 // 1.
 import { GiftedChat } from "react-native-gifted-chat";
+import Fire from "../components/Fire";
+
 class Chat extends React.Component {
   // 2.
   static navigationOptions = ({ navigation }) => ({
@@ -11,9 +13,36 @@ class Chat extends React.Component {
     messages: []
   };
 
+  // 1.
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message)
+      }))
+    );
+  }
+  // 2.
+  componentWillUnmount() {
+    Fire.shared.off();
+  }
+
+  get user() {
+    // Return our name and our UID for GiftedChat to parse
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid
+    };
+  }
+
   render() {
     // 4.
-    return <GiftedChat messages={this.state.messages} />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
+      />
+    );
   }
 }
 export default Chat;
