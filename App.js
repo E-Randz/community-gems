bc494842dfc4251a66cd24caac0504faf9680aca
Emma Randall <eerandall1@gmail.com>
2019-02-22 11:55:00 +0000

    tidy App.js

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import AuthLoadingScreen from "./src/components/screens/AuthLoadingScreen";
 import AuthNavigator from "./src/navigation/AuthNavigator";
 import firebase from "firebase";
 import { firebaseConfig } from "./config";
-import { postNewEvent, editEvent } from './src/db/events'
 
 firebase.initializeApp(firebaseConfig);
 
@@ -21,18 +20,11 @@ const switchNavigator = createSwitchNavigator(
     Auth: AuthNavigator
   },
   {
-    initialRouteName: "App"
+    initialRouteName: 'AuthLoading',
   }
 );
 
 class App extends Component {
-
-  // componentDidMount() {
-  //   console.log('hi there');
-  //   editEvent('-LZFraAJkCILvfhL4HOG', 'noice', '12 arlington drive', 'stockport', 'sk27eb', 'street clean', 'this event has updated', Date.now(), Date.now(), 3, 'Small job')
-  // }
-
-  state = {};
   render() {
     const AppContainer = createAppContainer(switchNavigator);
     return <AppContainer />;
