50251394b3d7886bbe6fc1f3f4b568ec720ab9c9
Emma Randall <eerandall1@gmail.com>
2019-02-22 11:56:20 +0000

    change tabNavigator to component that returns a navigator

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,14 +1,42 @@
-import { createBottomTabNavigator } from "react-navigation";
+import React, { Component } from 'react';
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import CreateEventScreen from "../components/screens/CreateEventScreen";
 import messageNav from "../navigation/MessageNav";
 import eventsList from "../components/screens/EventsListScreen";
 import HomeNavigator from "./HomeNavigator";
+import { getUserByID } from '../db/users';
+import firebase from 'firebase';
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeNavigator,
-  Events: eventsList,
-  "New Event": CreateEventScreen,
-  Messages: messageNav
-});
 
+class TabNavigator extends Component {
+  state = { 
+    user: null,
+  }
+
+  componentDidMount() {
+    const userID = firebase.auth().currentUser.uid;
+    getUserByID(userID)
+      .then((snapshot) => {
+        const user = snapshot.val();
+        this.setState({
+          user
+        })
+      })
+  }
+
+  render() { 
+    const TabNavigation = createBottomTabNavigator({
+      Home: HomeNavigator,
+      Events: eventsList,
+      "New Event": CreateEventScreen,
+      Messages: messageNav
+    });
+    const TabContainer = createAppContainer(TabNavigation);
+    return ( 
+      <TabContainer />
+    );
+  }
+}
+ 
 export default TabNavigator;
+
