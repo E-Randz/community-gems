cbd01d0b9ca1cc23f666a6b68f36dde8b89e49bd
Emma Randall <eerandall1@gmail.com>
2019-02-22 08:45:35 +0000

    create editEent function

--- src/db/events.js
+++ src/db/events.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase'
 import getCoords from '../utils'
 
-export const postNewEvent = (name, firstLineOfAddress, town, postcode, type, description, dateTime, createdDate, noOfVolunteers, timeScale, creatorUsername, creatorUid, participants ) => {
+export const postNewEvent = (name, firstLineOfAddress, town, postcode, type, description, dateTime, createdDate, noOfVolunteers, timeScale, creatorUsername, creatorUid) => {
   const address = `${firstLineOfAddress}+${town}+${postcode}`
   getCoords(address)
   .then((res) => {
     const lat = res.data.results[0].geometry.location.lat
     const long = res.data.results[0].geometry.location.lng
-
     const postEventData = {
       name,
       firstLineOfAddress,
@@ -25,9 +24,32 @@ export const postNewEvent = (name, firstLineOfAddress, town, postcode, type, des
       long,
 
     }
-
     firebase.database().ref('/Events').push(postEventData)
-      
   })
+}
 
+export const editEvent =  (eventID, name, firstLineOfAddress, town, postcode, type, description, dateTime, createdDate, noOfVolunteers, timeScale) => {
+  const address = `${firstLineOfAddress}+${town}+${postcode}`
+  getCoords(address)
+  .then((res) => {
+    const lat = res.data.results[0].geometry.location.lat
+    const long = res.data.results[0].geometry.location.lng
+    const updatedData = {
+      name,
+      firstLineOfAddress,
+      town,
+      postcode,
+      type,
+      description,
+      dateTime,
+      createdDate,
+      noOfVolunteers,
+      timeScale,
+      lat,
+      long,
+
+    }
+    firebase.database().ref(`/Events/${eventID}`).update(updatedData)
+  })
+  .catch(console.log);
 }
