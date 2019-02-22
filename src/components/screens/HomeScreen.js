9f9bd015968f229352e3544a86763f46998edbe0
Liamriley123 <43750285+Liamriley123@users.noreply.github.com>
2019-02-22 14:12:52 +0000

    styled homepage (#32)

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -7,46 +7,12 @@ import {
   Image,
   ScrollView,
   TouchableOpacity
-} from 'react-native'
+} from "react-native";
+import { ListItem, ButtonGroup } from "react-native-elements";
+
 
 import firebase from 'firebase'
 
-// import { db } from "../../config/db";
-// const db = firebase.database();
-
-// let userRef = db.ref("/Users");
-
-// class HomeScreen extends Component {
-//   state = {
-//     users: []
-//   }
-
-//   componentDidMount () {
-//     // userRef.on("value", snapshot => {
-//     //   let data = snapshot.val();
-//     //   let users = Object.values(data);
-//     //   this.setState({
-//     //     users
-//     //   });
-//     // });
-//   }
-
-//   render () {
-//     return
-//       <Text>Hello</Text>
-//   }
-// }
-
-// export default HomeScreen
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center'
-//   }
-// })
 
 class HomeScreen extends Component {
   state = {
@@ -90,29 +56,36 @@ class HomeScreen extends Component {
         eventOrganizer: 'user'
       }
     ],
+    selectedIndex: 0,
     pastEvent: false
-  }
+  };
+
+
+  
 
   render () {
-    const { upcoming, attended, pastEvent } = this.state
+    const { upcoming, attended, pastEvent, events } = this.state
+    const buttons = ["Upcoming", "Attended"];
+    const { selectedIndex } = this.state;
 
     return (
       <ScrollView>
+        <View
+          style={{
+            paddingTop: 80,
+            backgroundColor: "#00BFFF",
+            alignItems: "center"
+          }}
+        />
+        <Text style={styles.title}>HOME</Text>
         <View style={styles.container}>
           <View style={styles.userInfoBox}>
-            <Text style={styles.userInfoName}>Tymmy123</Text>
-
             <View style={styles.userData}>
-              <Image
-                style={styles.userImage}
-                source={require('../../../assets/heart.jpg')}
-              />
+
               <View style={styles.userText}>
-                <Text style={styles.userText_content}>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s.
-                </Text>
+                <Text style={styles.homeText}>Welcome back user!</Text>
+                <Text style={styles.homeText}>You have 100 gems 💎</Text>
+                <Text style={styles.homeText}>You have 5 Upcoming Events</Text>
               </View>
             </View>
 
@@ -121,55 +94,55 @@ class HomeScreen extends Component {
                 style={styles.userInfoBox_buttons}
                 onPress={() => this.props.navigation.navigate('Leaderboard')}
               >
-                <Text>View Leaderboard</Text>
+                <Text>Leaderboard</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={styles.userInfoBox_buttons}
                 onPress={() => this.props.navigation.navigate('Profile')}
               >
-                <Text>View Profile</Text>
+                <Text>Profile</Text>
               </TouchableOpacity>
             </View>
           </View>
 
-          <View style={styles.buttonsBox}>
-            <TouchableOpacity
-              style={styles.eventButtons}
-              onPress={this.handleUpcoming}
-            >
-              <Text>Upcoming</Text>
-            </TouchableOpacity>
+          <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            containerStyle={{ height: 50 }}
+          />
 
-            <TouchableOpacity
-              style={styles.eventButtons}
-              onPress={this.handleAttended}
-            >
-              <Text>Attended</Text>
-            </TouchableOpacity>
-          </View>
+          <View>
+            {selectedIndex ? upcoming.map((event, i) => (
+              <ListItem
+                key={i}
+                leftAvatar={{
+                  source: {
+                    uri: "https://bootdey.com/img/Content/avatar/avatar6.png"
+                  }
+                }}
+                title={event.title}
+                subtitle={`${event.start.slice(0, 10)}\n${
+                  event.location
+                }\nOrganizer :${event.eventOrganizer}`}
+                style={styles.reviewBox}
+              />
+            )) : attended.map((event, i) => (
+              <ListItem
+                key={i}
+                leftAvatar={{
+                  source: {
+                    uri: "https://bootdey.com/img/Content/avatar/avatar6.png"
+                  }
+                }}
+                title={event.title}
+                subtitle={`${event.start.slice(0, 10)}\n${
+                  event.location
+                }\nOrganizer :${event.eventOrganizer}`}
+                style={styles.reviewBox}
+              />
+            ))}
 
-          <View style={styles.eventsList}>
-            {pastEvent
-              ? upcoming.map((event, i) => (
-                <TouchableOpacity style={styles.eventParent} key={i}>
-                  <Text style={styles.eventTitle}>title: {event.title}</Text>
-                  <View style={styles.eventDetails}>
-                    <Text>{event.start}</Text>
-                    <Text>{event.location}</Text>
-                    <Text>By: {event.eventOrganizer}</Text>
-                  </View>
-                </TouchableOpacity>
-              ))
-              : attended.map((event, i) => (
-                <TouchableOpacity style={styles.eventParent} key={i}>
-                  <Text style={styles.eventTitle}>title: {event.title}</Text>
-                  <View style={styles.eventDetails}>
-                    <Text>{event.start}</Text>
-                    <Text>{event.location}</Text>
-                    <Text>By: {event.eventOrganizer}</Text>
-                  </View>
-                </TouchableOpacity>
-              ))}
           </View>
         </View>
       </ScrollView>
@@ -190,13 +163,6 @@ class HomeScreen extends Component {
 export default HomeScreen
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    marginTop: 80
-  },
 
   userInfoBox: {
     flexDirection: 'column',
@@ -207,30 +173,42 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     fontSize: 35,
     marginLeft: 40,
-    color: 'grey',
-    fontWeight: 'bold'
+    color: "grey",
+    fontWeight: "bold"
+
   },
 
   userData: {
     flexDirection: 'row'
   },
 
-  userImage: {
-    width: 150,
-    height: 120,
-    margin: 10,
-    borderRadius: 16
+  reviewBox: {
+    fontSize: 6,
+    backgroundColor: "#00BFFF",
+    fontWeight: "600",
+    borderColor: "#00BFFF",
+    borderBottomWidth: 2,
+    marginTop: 2
+  },
+
+  homeText: {
+    textAlign: "center",
+    fontSize: 20
   },
 
   userText: {
-    width: 220,
-    height: 120,
-    margin: 10
+    width: "100%",
+    height: 150,
+    textAlign: "center"
   },
 
   userText_content: {
-    justifyContent: 'space-evenly',
-    color: 'grey'
+    justifyContent: "space-evenly",
+    color: "grey",
+    borderWidth: 1,
+    borderColor: "#00BFFF",
+    marginRight: 10
+
   },
 
   buttonsBox: {
@@ -241,35 +219,40 @@ const styles = StyleSheet.create({
   },
 
   userInfoBox_buttons: {
-    backgroundColor: 'lightblue',
+    backgroundColor: "#00BFFF",
     padding: 12,
-    margin: 16,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: -40,
+    marginRight: 5,
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 4,
-    borderColor: 'rgba(0, 0, 0, 0.1)',
-    width: '40%'
+    borderWidth: 1,
+    borderColor: "black",
+    width: "40%"
   },
 
   eventButtons: {
-    flexDirection: 'row',
-    backgroundColor: 'lightblue',
+    flexDirection: "row",
+    backgroundColor: "#00BFFF",
     padding: 12,
-    // margin: 16,
-    justifyContent: 'center',
-    alignItems: 'center',
+    margin: 5,
+    justifyContent: "center",
+    alignItems: "center",
+
     borderRadius: 4,
     // borderColor: 'rgba(0, 0, 0, 0.1)',
     width: '48.5%',
     borderWidth: 2,
-    borderColor: 'blue'
+    borderColor: "black"
   },
 
   eventsList: {
-    width: '90%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'lightpink'
+    width: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00BFFF",
+    marginTop: 20
+
   },
 
   eventParent: {
@@ -281,59 +264,20 @@ const styles = StyleSheet.create({
   eventTitle: {
     fontWeight: 'bold'
   },
+  title: {
+    fontSize: 30,
+    color: "white",
+    backgroundColor: "#00BFFF",
+    paddingBottom: 10,
+    fontFamily: "Futura",
+    textAlign: "center",
+    marginBottom: 20
+  },
   eventDetails: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly'
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    padding: 20,
+    backgroundColor: "blue"
   }
-})
+});
 
-// events: [
-// {
-//   title: 'event1',
-//   start: '2010-01-09T12:30:00',
-//   location: 'manchester',
-//   eventOrganizer: 'tom'
-// },
-// {
-//   title: 'event2',
-//   start: '2010-01-09T18:30:00',
-//   location: 'salford',
-//   eventOrganizer: 'peter'
-// }
-// {
-//   title: 'event1',
-//   start: '2010-01-09T12:30:00',
-//   location: 'manchester',
-//   eventOrganizer: 'user'
-// },
-// {
-//   title: 'event1',
-//   start: '2010-01-09T12:30:00',
-//   location: 'manchester',
-//   eventOrganizer: 'user'
-// },
-// {
-//   title: 'event1',
-//   start: '2010-01-09T12:30:00',
-//   location: 'manchester',
-//   eventOrganizer: 'user'
-// },
-// {
-//   title: 'event1',
-//   start: '2010-01-09T12:30:00',
-//   location: 'manchester',
-//   eventOrganizer: 'user'
-// },
-// {
-//   title: 'event1',
-//   start: '2010-01-09T12:30:00',
-//   location: 'manchester',
-//   eventOrganizer: 'user'
-// },
-// {
-//   title: 'event1',
-//   start: '2010-01-09T12:30:00',
-//   location: 'manchester',
-//   eventOrganizer: 'user'
-// },
-// ]
