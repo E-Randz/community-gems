222847f5fcb2d94b5da5f5a196cfa345693fd818
John Nezzi <john@Johns-MacBook-Pro.local>
2019-02-22 10:51:12 +0000

    create a basic map component

---
+++ src/components/map.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { styleSheet, Text, View } from 'react-native'
+import { MapViewAnimated } from 'react-native-maps';
+
+const Marker = MapView.Marker
+
+export default class Map extends Component {
+
+  state = {
+    places: { name: streetclean }
+  }
+
+  renderMarkers() {
+    return this.state.places.map((place, i) => (
+      <Marker key={i} title={place.name} coordinate={place.coords} />
+    ))
+  }
+
+  render() {
+    const { region } = this.state;
+
+    return (
+      <MapView
+        style={styles.container}
+        region={region}
+        showsUserLocation
+        ShowsMyLocationButton
+        >
+
+        {this.renderMarkers()}
+      </MapView>
+    )
+
+    const styles = {
+      container: {
+        width: '100%',
+        height: '80%'
+
+      }
+    }
+
+  }
+
+}
