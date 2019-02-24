2440d437dfec6686cd47fa456227e3403ced1f72
Liamriley123 <43750285+Liamriley123@users.noreply.github.com>
2019-02-24 09:34:02 +0000

    add reviews to users (#37)

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { Text, View } from "react-native";
 import {
   createBottomTabNavigator,
   createAppContainer,
   createSwitchNavigator
-
 } from "react-navigation";
 import TabNavigator from "./src/navigation/TabNavigator";
 import AuthLoadingScreen from "./src/components/screens/AuthLoadingScreen";
 import AuthNavigator from "./src/navigation/AuthNavigator";
 import firebase from "firebase";
 import { firebaseConfig } from "./config";
-import { postNewEvent } from './src/db/events'
+import { addReview } from "./src/db/users";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -24,18 +23,24 @@ const switchNavigator = createSwitchNavigator(
   {
     initialRouteName: "AuthLoading"
   }
-)
+);
+
+addReview(
+  "0u42EIcHPAM2YdvNTXkEWo0iHLk1",
+  "hello this is another review",
+  "11/01/2000"
+);
 
 class App extends Component {
   // componentDidMount() {
   //   postNewEvent('testevent2', '12 arlington drive', 'stockport', 'sk27eb', 'street clean', 'great event', Date.now(), Date.now(), 3, 'Small job', 'vabbbzbsbs', 'VsRmfX5PtQRP7KIJ2yStGYGKJef2')
   // }
 
-  state = {}
-  render () {
-    const AppContainer = createAppContainer(switchNavigator)
-    return <AppContainer />
+  state = {};
+  render() {
+    const AppContainer = createAppContainer(switchNavigator);
+    return <AppContainer />;
   }
 }
 
-export default App
+export default App;
