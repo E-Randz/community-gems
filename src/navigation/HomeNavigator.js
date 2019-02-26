72053d34dce6e840b0e02424d3a2c33d71b492c2
Emma Randall <eerandall1@gmail.com>
2019-02-26 16:41:39 +0000

    add event list screen to event view

--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -3,13 +3,15 @@ import HomeScreen from "../components/screens/HomeScreen";
 import ProfileScreen from "../components/screens/ProfileScreen";
 import OtherProfileScreen from "../components/screens/OtherProfileScreen";
 import LeaderboardScreen from "../components/screens/LeaderboardScreen";
+import EventViewOrganiser from "../components/screens/EventViewOrganiser";
 
 const HomeNavigator = createStackNavigator(
   {
     Home: HomeScreen,
     Profile: ProfileScreen,
     OtherProfile: OtherProfileScreen,
-    Leaderboard: LeaderboardScreen
+    Leaderboard: LeaderboardScreen,
+    EventView: EventViewOrganiser,
   },
   {
     initialRouteName: "Home"
