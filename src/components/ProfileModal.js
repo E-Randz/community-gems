398b8486d7b6c372a57cbc6008489a33d1380e08
Emma Randall <eerandall1@gmail.com>
2019-03-05 14:54:08 +0000

    add Proptypes to profile modal

--- src/components/ProfileModal.js
+++ src/components/ProfileModal.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, Text, ScrollView, View, StyleSheet } from 'react-native';
 import { Input } from './Input';
-
-
+import PropTypes from 'prop-types';
 
 const ProfileModal = ({ props }) => {
   const { 
@@ -17,7 +16,6 @@ const ProfileModal = ({ props }) => {
           postcode
         } = props;
 
-        console.log(props, 'here');
   return (
     <ScrollView>
       <View style={styles.modalContent}>
@@ -42,25 +40,21 @@ const ProfileModal = ({ props }) => {
         />
         <Text>Change address</Text>
         <Input
-          style={styles.input}
           placeholder="House Number"
           onChangeText={houseNo => updateInput("houseNo", houseNo)}
           value={houseNo}
         />
         <Input
-          style={styles.input}
           placeholder="Street"
           onChangeText={street => updateInput("street", street)}
           value={street}
         />
         <Input
-          style={styles.input}
           placeholder="Town"
           onChangeText={town => updateInput("town", town)}
           value={town}
         />
         <Input
-          style={styles.input}
           placeholder="Postcode"
           onChangeText={postcode => updateInput("postcode", postcode)}
           value={postcode}
@@ -82,82 +76,32 @@ const ProfileModal = ({ props }) => {
 
 export default ProfileModal;
 
+ProfileModal.PropTypes = {
+  onChangeImagePress: PropTypes.func.isRequired, 
+  updateInput: PropTypes.func.isRequired,
+  saveProfileChanges: PropTypes.func.isRequired,
+  setUserInputs: PropTypes.func.isRequired,
+  description: PropTypes.string.isRequired, 
+  houseNo: PropTypes.string.isRequired, 
+  street: PropTypes.string.isRequired, 
+  town: PropTypes.string.isRequired, 
+  postcode: PropTypes.string.isRequired
+}
+
 const styles = StyleSheet.create({
-  header: {
-    backgroundColor: "#00BFFF",
-    height: 200
-  },
-
-  buttonBox: {
-    flex: 1,
-    alignItems: "center"
-  },
-
-  avatar: {
-    width: 130,
-    height: 130,
-    borderRadius: 40,
-    borderWidth: 4,
-    borderColor: "white",
-    marginBottom: 10,
-    alignSelf: "center",
-    position: "absolute",
-    marginTop: 130
-  },
-  name: {
-    fontSize: 22,
-    color: "black",
-    fontWeight: "600"
-  },
-  reviewBox: {
-    fontSize: 6,
-    backgroundColor: "#00BFFF",
-    fontWeight: "600",
-    borderColor: "#00BFFF",
-    borderWidth: 2,
-    marginTop: 2
-  },
-  //   reviewHolder: {
-  //     backgroundColor: "#00BFFF"
-  //   },
-
-  body: {
-    marginTop: 70,
-    alignItems: "center"
-    // borderColor: "#00BFFF",
-    // borderWidth: 2,
-    // borderRadius: 13
-  },
-
-  info: {
-    fontSize: 20,
-
-    color: "#00BFFF",
-    marginTop: 10
-  },
-  description: {
-    fontSize: 16,
-    color: "black",
-
-    marginTop: 10,
-    textAlign: "center"
-  },
-  buttonContainer: {
-    marginTop: 10,
-    height: 25,
-
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 20,
-    width: 120,
-
-    borderRadius: 30,
-    backgroundColor: "#00BFFF"
-  },
   modalContent: {
     backgroundColor: "white",
     padding: 22,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    borderColor: "rgba(0, 0, 0, 0.1)"
+  },
+  button2: {
+    backgroundColor: "#00BFFF",
+    height: 50,
+    width: 200,
+    margin: 5,
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
@@ -172,16 +116,4 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderColor: "rgba(0, 0, 0, 0.1)"
   },
-  button2: {
-    backgroundColor: "#00BFFF",
-    height: 50,
-    width: 200,
-    // padding: 15,
-    // fontSize: 50,
-    margin: 5,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 4,
-    borderColor: "rgba(0, 0, 0, 0.1)"
-  }
 });
